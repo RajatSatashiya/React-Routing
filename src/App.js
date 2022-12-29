930518ea3d93fee2ae9817e916511b3a6410e4dc
Rajat Satashiya <kshri4398@gmail.com>
2022-12-29 16:20:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import Error from "./Components/Error";
+import Products from "./Components/Products";
+import Product from "./Components/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Products.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function Products() {
+  const [products, setProducts] = useState([]);
+  const getData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/products");
+      const data = await response.json();
+      setProducts(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const displayItems = products.map((product, index) => (
+    <div key={index} className="card">
+      <h1>{product.name}</h1>
+      <p>Price: ${product.price}</p>
+      <button>
+        <Link to={`/product/${product.id}`}>More Details</Link>
+      </button>
+    </div>
+  ));
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <>{displayItems}</>;
+}
+
+export default Products;
